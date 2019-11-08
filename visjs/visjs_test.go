a3a630433fe2c384101648640593dc19ca16d8e0
Björn Gylling <bjorn.gylling@gmail.com>
2019-11-08 19:00:12 +0100

    Sort the node slice in the test to avoid order breaking tests

--- visjs/visjs_test.go
+++ visjs/visjs_test.go
@@ -3,6 +3,8 @@ package visjs
 import (
 	"github.com/bjorngylling/kafka-acl-viewer/graph"
 	"reflect"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -62,6 +64,15 @@ func Test_createNetwork(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, got1 := CreateNetwork(tt.args.graph)
+
+			// sort to since the order is not important
+			sort.Slice(got, func(i, j int) bool {
+				return strings.Compare(got[i].ID, got[j].ID) < 0
+			})
+			sort.Slice(tt.wantNodes, func(i, j int) bool {
+				return strings.Compare(tt.wantNodes[i].ID, tt.wantNodes[j].ID) < 0
+			})
+
 			if !reflect.DeepEqual(got, tt.wantNodes) {
 				t.Errorf("createNetwork() got = %v, wantNodes %v", got, tt.wantNodes)
 			}
